19eddc5548ccf560650260ebcd4bcde01c082cbe
Chris Kuffert <chriskuffert@comcast.net>
2017-09-27 12:45:53 -0400

    New solutions

--- Solutions.cs
+++ Solutions.cs
@@ -8,6 +8,11 @@ namespace CodingProblems
 {
     public class Solutions
     {
+        /// <summary>
+        /// Reverses an integer.
+        /// </summary>
+        /// <param name="x">Integer to be reversed</param>
+        /// <returns>The reversed integer</returns>
         public static int reverse(int x)
         {
             long result = 0;
@@ -20,6 +25,11 @@ namespace CodingProblems
             return (result > Int32.MaxValue || result < Int32.MinValue) ? 0 : (int)result;
         }
 
+        /// <summary>
+        /// Determines if the integer is a palindrome.
+        /// </summary>
+        /// <param name="x">Integer to be checked</param>
+        /// <returns>Whether the passed integer is indeed a palindrome</returns>
         public bool IsPalindrome(int x)
         {
             if (x < 0)
@@ -39,6 +49,12 @@ namespace CodingProblems
             return true;    
         }
 
+        /// <summary>
+        /// Adds to reverse-ordered linked lists of integers.
+        /// </summary>
+        /// <param name="l1">The first reverse-ordered linked list</param>
+        /// <param name="l2">The second reverse-ordered linked list</param>
+        /// <returns>A reverse-ordered linked list of the sum of the two linked lists</returns>
         public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
         {
             int remainder = 0;
@@ -75,5 +91,108 @@ namespace CodingProblems
 
             return dummyHead.next;
         }
+
+        /// <summary>
+        /// Converts Roman Numerals to an integer value.
+        /// </summary>
+        /// <param name="s">The string representing a series of Roman Numerals</param>
+        /// <returns>The integer value of the series of Roman Numerals</returns>
+        public int RomanToInt(string s)
+        {
+            int sum = 0, valToAdd = 0, lastValToAdd = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case ('I'):
+                        valToAdd = 1;
+                        break;
+                    case ('V'):
+                        valToAdd = 5;
+                        break;
+                    case ('X'):
+                        valToAdd = 10;
+                        break;
+                    case ('L'):
+                        valToAdd = 50;
+                        break;
+                    case ('C'):
+                        valToAdd = 100;
+                        break;
+                    case ('D'):
+                        valToAdd = 500;
+                        break;
+                    case ('M'):
+                        valToAdd = 1000;
+                        break;
+                }
+                if (valToAdd > lastValToAdd && lastValToAdd != 0)
+                {
+                    sum -= lastValToAdd * 2;
+                    sum += valToAdd;
+                }
+                else
+                {
+                    sum += valToAdd;
+                }
+                lastValToAdd = valToAdd;
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// Finds the longest prefix from a list of strings
+        /// </summary>
+        /// <param name="strs">The list of strings</param>
+        /// <returns>The longest prefix</returns>
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string lp = "";
+            if (strs.Length <= 0) { return lp; }
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                char c = strs[0][i];
+                bool addChar = true;
+                for (int k = 1; k < strs.Length; k++)
+                {
+                    if (i >= strs[k].Length)
+                    {
+                        addChar = false;
+                        break;
+                    }
+                    if (strs[k][i] != c)
+                    {
+                        addChar = false;
+                        break;
+                    }
+                }
+                if (addChar) { lp += c; continue; }
+                break;
+            }
+            return lp;
+        }
+
+        /// <summary>
+        /// Removes duplicates from an array of integers, shifting duplicates to the back
+        /// and returning the new length of non-dupe integers.
+        /// </summary>
+        /// <param name="nums">Array of ints to remove dupes from</param>
+        /// <returns>Length of non-dupe array</returns>
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2) { return nums.Length; }
+            int i = 0, j;
+            for (j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    nums[++i] = nums[j];
+                    continue;
+                }
+            }
+            return ++i;
+        }
     }
+
+    
 }
